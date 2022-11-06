b1b563f49bd28cdf06aacb18b387e9486d80853d
Fabiola Horas <fabiolahoras@gmail.com>
2022-11-06 13:13:48 -0300

    Detalles sobre el juego BlackJack y desarrollo de clase Dados

--- 00-Blackjack.ts
+++ 00-Blackjack.ts
@@ -24,7 +24,7 @@ export class Blackjack {
   public jugarBlackjack(pJugador: Jugador) {
     console.clear();
     console.log("------------------------------------------------------------------------------------")
-    console.log("<Bienvenido al juego de Blackjack>")
+    console.log("< Bienvenido al juego de Blackjack >")
     console.log("------------------------------------------------------------------------------------")
 
     let continuarBlackjack: boolean = true;
@@ -39,7 +39,7 @@ export class Blackjack {
       while (sumaJugador < 21 && continuarJugador === true) {
         if (contadorVueltas === 1) {
           console.log("------------------------------------------------------------------------------------")
-          console.log(">Inicia la mano Jugador");
+          console.log("> Inicia la mano Jugador");
           console.log("-------------------------Vuelta Jugador numero " + contadorVueltas + "---------------------------------------")
         } else {
           console.log("-------------------------Vuelta Jugador numero " + contadorVueltas + "--------------------------------------")
@@ -49,80 +49,80 @@ export class Blackjack {
           case 1:
             console.log(">Ha salido el As (Puede valer 1 u 11)")
             if (readlineSync.keyInYN('Presione Y = 1, o N = 11')) {
-              console.log(">Ha elejido hacer valer el As 1")
+              console.log("> Ha elegido hacer valer el As 1")
               sumaJugador += 1
             } else {
-              console.log(">Ha elejido hacer valer el As 11")
+              console.log("> Ha elegido hacer valer el As 11")
               sumaJugador += 11
             }
             break;
 
           case 2:
-            console.log(">Carta 2")
+            console.log("> Carta 2")
             sumaJugador += 2
             break;
 
           case 3:
-            console.log(">Carta 3")
+            console.log("> Carta 3")
             sumaJugador += 3
             break;
 
           case 4:
-            console.log(">Carta 4")
+            console.log("> Carta 4")
             sumaJugador += 4
             break;
 
           case 5:
-            console.log(">Carta 5")
+            console.log("> Carta 5")
             sumaJugador += 5
             break;
 
           case 6:
-            console.log(">Carta 6")
+            console.log("> Carta 6")
             sumaJugador += 6
             break;
 
           case 7:
-            console.log(">Carta 7")
+            console.log("> Carta 7")
             sumaJugador += 7
             break;
 
           case 8:
-            console.log(">Carta 8")
+            console.log("> Carta 8")
             sumaJugador += 8
             break;
 
           case 9:
-            console.log(">Carta 9")
+            console.log("> Carta 9")
             sumaJugador += 9
             break;
 
           case 10:
-            console.log(">Carta 10")
+            console.log("> Carta 10")
             sumaJugador += 10
             break;
 
           case 11:
-            console.log(">Carta J (vale 10)")
+            console.log("> Carta J (vale 10)")
             sumaJugador += 10
             break;
 
           case 12:
-            console.log(">Carta Q (vale 10)")
+            console.log("> Carta Q (vale 10)")
             sumaJugador += 10
             break;
 
           case 13:
-            console.log(">Carta K (vale 10)")
+            console.log("> Carta K (vale 10)")
             sumaJugador += 10
             break;
         }
 
-        if (sumaJugador > 21) {//Jugador se pasa de 21. Automaticametne pierde
+        if (sumaJugador > 21) {//Jugador se pasa de 21. Automáticamente pierde
           console.log(`-->La sumaJugador total es: ${sumaJugador}`)
           console.log(">Usted ha perdido!")
           pJugador.modificarSaldoJugador(-100)
-          console.log("-->Al finalizar esta ronda su saldo de jugador ha quedado asi: " + pJugador.getSaldoJugador())
+          console.log("--> Al finalizar esta ronda su saldo de jugador ha quedado asi: " + pJugador.getSaldoJugador())
           continuarJugador = false; // >:(
           continuarCasino = false;// >:(
 
@@ -158,98 +158,98 @@ export class Blackjack {
 
         switch (getRandomInt(1, 14)) {
           case 1:
-            console.log(">Ha salido el As")
+            console.log("> Ha salido el As")
             if (sumaCasino + 11 <= 21) {
-              console.log("-->El Casino ha elejido hacer valer el As 11")
+              console.log("--> El Casino ha elejido hacer valer el As 11")
               sumaCasino += 11
             } else {
-              console.log("-->El Casino ha elejido hacer valer el As 1")
+              console.log("--> El Casino ha elejido hacer valer el As 1")
               sumaCasino += 1
             }
             break;
 
           case 2:
-            console.log(">Carta 2")
+            console.log("> Carta 2")
             sumaCasino += 2
             break;
 
           case 3:
-            console.log(">Carta 3")
+            console.log("> Carta 3")
             sumaCasino += 3
             break;
 
           case 4:
-            console.log(">Carta 4")
+            console.log("> Carta 4")
             sumaCasino += 4
             break;
 
           case 5:
-            console.log(">Carta 5")
+            console.log("> Carta 5")
             sumaCasino += 5
             break;
 
           case 6:
-            console.log(">Carta 6")
+            console.log("> Carta 6")
             sumaCasino += 6
             break;
 
           case 7:
-            console.log(">Carta 7")
+            console.log("> Carta 7")
             sumaCasino += 7
             break;
 
           case 8:
-            console.log(">Carta 8")
+            console.log("> Carta 8")
             sumaCasino += 8
             break;
 
           case 9:
-            console.log(">Carta 9")
+            console.log("> Carta 9")
             sumaCasino += 9
             break;
 
           case 10:
-            console.log(">Carta 10")
+            console.log("> Carta 10")
             sumaCasino += 10
             break;
 
           case 11:
-            console.log(">Carta J (vale 10)")
+            console.log("> Carta J (vale 10)")
             sumaCasino += 10
             break;
 
           case 12:
-            console.log(">Carta Q (vale 10)")
+            console.log("> Carta Q (vale 10)")
             sumaCasino += 10
             break;
 
           case 13:
-            console.log(">Carta K (vale 10)")
+            console.log("> Carta K (vale 10)")
             sumaCasino += 10
             break;
         }
 
-        console.log("-->En esta mano el casino suma " + sumaCasino)
+        console.log("--> En esta mano el casino suma " + sumaCasino)
 
         if (sumaCasino > 21) {//Casino se pasa de 21 y pierde
-          console.log(">El casino se paso, y usted gana!")
+          console.log("> El casino se paso, y usted gana!")
           pJugador.modificarSaldoJugador(100)
-          console.log("-->Al finalizar esta ronda su saldo de jugador ha quedado asi: " + pJugador.getSaldoJugador())
+          console.log("--> Al finalizar esta ronda su saldo de jugador ha quedado asi: " + pJugador.getSaldoJugador())
           continuarCasino = false;
 
         } else if (sumaCasino > sumaJugador && sumaCasino <= 21) {//Si el Casino sumas mas que el jugador, y menos de 21, gana
-          console.log(">El casino gano, lo lamento!")
+          console.log("> El casino gano, lo lamento!")
           pJugador.modificarSaldoJugador(-100)
-          console.log("-->Al finalizar esta ronda su saldo de jugador ha quedado asi: " + pJugador.getSaldoJugador())
+          console.log("--> Al finalizar esta ronda su saldo de jugador ha quedado asi: " + pJugador.getSaldoJugador())
           continuarCasino = false;
         } else {
           continuarCasino = true;
         }
 
         if (sumaCasino === 21 && sumaJugador === 21) {
-          console.log("Ha habido un empate!!!. De todas formas el Jugador gana!!!")
+          console.log(" EMPATE !!! El Jugador GANA !!!")
           pJugador.modificarSaldoJugador(100);
-          console.log("-->Al finalizar esta ronda su saldo de jugador ha quedado asi: " + pJugador.getSaldoJugador())
+          console.log("--> Al finalizar esta ronda su saldo de jugador ha quedado asi: " + pJugador.getSaldoJugador())
         }
 
         contadorVueltas++;
--- 00-Dados.ts
+++ 00-Dados.ts
@@ -1,3 +1,68 @@
+import { Jugador } from './00-Jugador';
+
+
+//-------Funcion Aleatorio para generar los numeros------
+function getRandomInt(min: number, max: number) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+}
+
 class Dados {
-    
+    private cantDados: number;
+    private sumaDados: number;
+    private pozoDados: number;
+    private apuestaMinimaDados: number;
+
+    constructor(pCantDados: number, pSumaDados: number, pPozoDados: number, pApuestaMinimaDados: number){
+        this.cantDados= pCantDados;
+        this.sumaDados= pSumaDados;
+        this.pozoDados= pPozoDados;
+        this.apuestaMinimaDados = pApuestaMinimaDados;
+    }
+
+    public getCantDados(): number {
+        return this.cantDados;
+    }
+
+    public setCantDados(cantDados: number): void {
+        this.cantDados = cantDados;
+    }
+
+    public getSumaDados(): number {
+        return this.sumaDados;
+    }
+
+    public setSumaDados(sumaDados: number): void {
+        this.sumaDados = sumaDados;
+    }
+
+    public getPozoDados(): number {
+        return this.pozoDados;
+    }
+
+    public setPozoDados(pozoDados: number): void {
+        this.pozoDados = pozoDados;
+    }
+
+    public getApuestaMinimaDados(): number {
+        return this.apuestaMinimaDados;
+    }
+
+    public setApuestaMinimaDados(apuestaMinimaDados: number): void {
+        this.apuestaMinimaDados = apuestaMinimaDados;
+    }
+
+    public lanzarDados(): number{
+        let dado: number = getRandomInt(1,6);
+        return dado; 
+    }
+
+    public jugarDados(){
+        let dado1: number= 0;
+        let dado2: number =0;
+        let suma: number= 0;
+
+
+    }
 }
